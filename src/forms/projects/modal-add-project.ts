9d18c91a81f0fb2243b4436361cf4923af934a0d
aasoto <andresalfredosotosuarez@gmail.com>
2023-01-19 22:45:17 -0500

    tomar valores de los campos del formulario

--- src/forms/projects/modal-add-project.ts
+++ src/forms/projects/modal-add-project.ts
@@ -10,5 +10,38 @@ export const showModalAddProject = (): void => {
     formProject(undefined, null);
     modalAddProject?.classList.toggle('hidden');
     cardProjectAdd?.classList.toggle('hidden');
+
+    const footerProjectAdd: HTMLElement | null = document.getElementById('footer-project-add');
+    while (footerProjectAdd?.firstChild) {
+      footerProjectAdd.removeChild(footerProjectAdd.firstChild);
+    }
+    
+    const btnSave: HTMLButtonElement = document.createElement('button');
+    btnSave.classList.add('btn-footer-success');
+    btnSave.id = 'send-new-project';
+    btnSave.textContent = 'Guardar';
+
+    btnSave.addEventListener('click', event => {
+      const sent: HTMLInputElement = (<HTMLInputElement>document.getElementById('sent'));
+
+      if (sent?.value == 'false') {
+        sent.value = 'true';
+
+        btnSave.classList.remove('btn-footer-success');
+        btnSave.classList.add('btn-footer-success-disable');
+
+        const title: HTMLInputElement = (<HTMLInputElement>document.getElementById('title'));
+        const description: HTMLTextAreaElement = (<HTMLTextAreaElement>document.getElementById('description'));
+        const startExecution: HTMLInputElement = (<HTMLInputElement>document.getElementById('start_execution'));
+        const endExecution: HTMLInputElement = (<HTMLInputElement>document.getElementById('end_execution'));
+        const contractorCompanyID: HTMLSelectElement = (<HTMLSelectElement>document.getElementById('contractor_company_id'));
+
+        console.log(title.value, description.value, startExecution.value, endExecution.value, contractorCompanyID.value);
+        
+      }
+    });
+
+    footerProjectAdd?.appendChild(btnSave);
+
   });
 }
